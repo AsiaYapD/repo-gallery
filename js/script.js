36a0885310c54694d1e579e96ba9be0738592de8
Asia Yap-Dubois <asiayapd@gmail.com>
2023-12-04 08:29:04 -0500

    Created API to pull in github info

--- js/script.js
+++ js/script.js
@@ -1,6 +1,13 @@
 
 /* Where my profile information will appear  */ 
 const overview = document.querySelector(".overview");
+const username = "AsiaYapD";
 
-/* const username = */
 
+const getUserInfo = async function() {
+    const userInfo = await fetch(`https://api.github.com/users/${username}`);
+    const data = await userInfo.json();
+    //console.log(data);
+
+};
+getUserInfo();
